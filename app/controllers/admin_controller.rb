f75e9d7999feeeb188cdb271735912612934d422
stevegriffin <stephenmgriffin80@gmail.com>
2015-02-09 20:27:18 -0500

    Added basic admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,19 +1,21 @@
 class AdminController < ApplicationController
 
   def index
+  	admin?
+  	render 'panel'
   end
 
   #authentication for admin user
   def authenticate
     if session[:admin] == true
-      render 'dashboard'
+      render 'panel'
     end
-
+    #binding.pry
     user = User.find_by(email: params[:email])
     if user && user.admin == true && user.authenticate(params[:password])
       session[:user_id] = user.id
       session[:admin] = user.admin
-      redirect_to admin_dashboard_path
+      redirect_to admin_panel_path
     else
       flash.now.alert = "Email or password is invalid"
       render plain: "Login failed"
